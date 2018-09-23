f1c610881ac4c2b9cfa50bba21a7f69a20e0fd10
Friedger <friedger@gmail.com>
2018-09-23 04:35:59 +0200

    copy frontend to frontend_gym

---
+++ frontend/src/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import * as paths from './paths';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header_container">
+        <ul className="header_progressbar">
+          <li
+            className={
+              paths.MEDICAL_INFORMATION_PATH === this.props.location.pathname
+                ? 'header_active'
+                : ''
+            }
+          >
+            Medical<br />information
+          </li>
+          <li
+            className={
+              paths.DERIVE_DATA_PATH === this.props.location.pathname
+                ? 'header_active'
+                : ''
+            }
+          >
+            Derive<br />data
+          </li>
+          <li
+            className={
+              paths.SCAN_MASTER_PASSWORD_PATH === this.props.location.pathname
+                ? 'header_active'
+                : ''
+            }
+          >
+            Scan master<br />password
+          </li>
+          <li
+            className={
+              paths.ENCRYPT_PATH === this.props.location.pathname
+                ? 'header_active'
+                : ''
+            }
+          >
+            Encrypt
+          </li>
+          <li
+            className={
+              paths.PUBLISH_PATH === this.props.location.pathname
+                ? 'header_active'
+                : ''
+            }
+          >
+            Publish
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Header);
---
+++ frontend_gym/src/paths.js
@@ -0,0 +1,6 @@
+export const MEDICAL_INFORMATION_PATH = "/";
+export const DERIVE_DATA_PATH = "/derive_data";
+export const SCAN_MASTER_PASSWORD_PATH = "/scan_master_password";
+export const ENCRYPT_PATH = "/encrypt";
+export const PUBLISH_PATH = "/publish";
+
