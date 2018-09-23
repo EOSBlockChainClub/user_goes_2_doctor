706205ab7945d3fbd65ae4d42a9dd934276d6730
José Luis Landabaso Díaz <lbo@troy.local>
2018-09-23 12:33:20 +0200

    Better format

--- frontend_gym/src/Header.js
+++ frontend_gym/src/Header.js
@@ -6,28 +6,30 @@ import './Header.css';
 class Header extends React.Component {
   render() {
     return (
-      <div className="header_container">
-      <center>MyGym</center>
-        <ul className="header_progressbar">
-          <li
-            className={
-              paths.INFORMATION_REQUEST_PATH === this.props.location.pathname
-                ? 'header_active'
-                : ''
-            }
-          >
-            Information<br />Request
-          </li>          
-          <li
-            className={
-              paths.VERIFY_PATH === this.props.location.pathname
-                ? 'header_active'
-                : ''
-            }
-          >
-            Verify
-          </li>      
-        </ul>
+      <div>
+        <center>Gym</center>
+        <div className="header_container">
+          <ul className="header_progressbar">
+            <li
+              className={
+                paths.INFORMATION_REQUEST_PATH === this.props.location.pathname
+                  ? 'header_active'
+                  : ''
+              }
+            >
+              Information<br />Request
+            </li>
+            <li
+              className={
+                paths.VERIFY_PATH === this.props.location.pathname
+                  ? 'header_active'
+                  : ''
+              }
+            >
+              Verify
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
--- frontend_gym/src/InformationRequest.js
+++ frontend_gym/src/InformationRequest.js
@@ -3,16 +3,16 @@ import WrappedLink from './WrappedLink';
 import * as paths from './paths';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
-import * as verify from './verifyFunctions'
+import * as verify from './verifyFunctions';
 class InformationRequest extends React.Component {
   render() {
+    //<img src="images/qr-code.png" />
     return (
       <div>
         <div>YourGym needs certificate for</div>
         <div>
-        Is the subject in good condition to exercise?            
+          <h2>Is the subject in good condition to exercise?</h2>
         </div>
-        <img src="images/qr-code.png"/>
         <center>
           <WrappedLink
             to={paths.VERIFY_PATH}
